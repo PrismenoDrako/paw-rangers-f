0b94b5d97d792b93c54b9fbe62b381c72315d224
Raul Seminario <raulsem7@gmail.com>
2025-11-13 03:04:04 -0500

    notifications

--- src/app/features/profile/profile.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const profileRoutes: Routes = [
-{
-    path: 'perfil',
-    loadComponent: () => import('./pages/profile').then((m) => m.Profile),
-    },
-];
